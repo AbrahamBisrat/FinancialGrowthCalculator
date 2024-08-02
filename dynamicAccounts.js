cb8c2b1789abfbcd265f6827ad39225bf9326c5b
Abraham Bisrat <30749258+AbrahamBisrat@users.noreply.github.com>
2024-08-02 10:15:19 -0500

    more refactored.

---
+++ dynamicAccounts.js
@@ -0,0 +1,77 @@
+let accountCounter = 0;
+
+function addAccount() {
+    accountCounter++;
+    const accountHTML = `
+        <div class="account" id="account-${accountCounter}">
+            <h4 class="section-title">Account ${accountCounter} <button type="button" class="btn red btn-remove" onclick="removeAccount(${accountCounter})">Remove</button></h4>
+            <div class="row">
+                <div class="input-field col s12 m6">
+                    <input type="text" id="accountLabel-${accountCounter}" value="Account ${accountCounter}" class="validate">
+                    <label for="accountLabel-${accountCounter}">Label</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="number" id="initialBalance-${accountCounter}" value="0" class="validate">
+                    <label for="initialBalance-${accountCounter}">Initial Balance ($)</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="number" id="annualContribution-${accountCounter}" value="0" class="validate">
+                    <label for="annualContribution-${accountCounter}">Annual Contribution ($)</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="number" step="0.01" id="annualReturnRate-${accountCounter}" value="7" class="validate">
+                    <label for="annualReturnRate-${accountCounter}">Annual Return Rate (%)</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="number" id="contributionIncrease-${accountCounter}" value="0" class="validate">
+                    <label for="contributionIncrease-${accountCounter}">Annual Contribution Increase ($)</label>
+                </div>
+            </div>
+        </div>
+    `;
+    document.getElementById('accountsContainer').insertAdjacentHTML('beforeend', accountHTML);
+}
+
+function removeAccount(accountId) {
+    document.getElementById(`account-${accountId}`).remove();
+}
+
+function populateSampleData() {
+    document.getElementById('accountsContainer').innerHTML = ''; // Clear existing accounts
+    accountCounter = 0;
+    addSampleAccount('Roth IRA', 0, 6000, 7);
+    addSampleAccount('401k', 10000, 19500, 7);
+    addSampleAccount('Brokerage', 5000, 10000, 7, 500);
+}
+
+function addSampleAccount(label, initialBalance, annualContribution, annualReturnRate, contributionIncrease = 0) {
+    accountCounter++;
+    const accountHTML = `
+        <div class="account" id="account-${accountCounter}">
+            <h4 class="section-title">${label} <button type="button" class="btn red btn-remove" onclick="removeAccount(${accountCounter})">Remove</button></h4>
+            <div class="row">
+                <div class="input-field col s12 m6">
+                    <input type="text" id="accountLabel-${accountCounter}" value="${label}" class="validate">
+                    <label for="accountLabel-${accountCounter}">Label</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="number" id="initialBalance-${accountCounter}" value="${initialBalance}" class="validate">
+                    <label for="initialBalance-${accountCounter}">Initial Balance ($)</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="number" id="annualContribution-${accountCounter}" value="${annualContribution}" class="validate">
+                    <label for="annualContribution-${accountCounter}">Annual Contribution ($)</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="number" step="0.01" id="annualReturnRate-${accountCounter}" value="${annualReturnRate}" class="validate">
+                    <label for="annualReturnRate-${accountCounter}">Annual Return Rate (%)</label>
+                </div>
+                <div class="input-field col s12 m6">
+                    <input type="number" id="contributionIncrease-${accountCounter}" value="${contributionIncrease}" class="validate">
+                    <label for="contributionIncrease-${accountCounter}">Annual Contribution Increase ($)</label>
+                </div>
+            </div>
+        </div>
+    `;
+    document.getElementById('accountsContainer').insertAdjacentHTML('beforeend', accountHTML);
+}
